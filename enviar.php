4f4eb6d4e726ea15bad20c9562129d64689092ca
keilacardodo <keila_dcardoso@hotmail.com>
2021-06-20 12:19:58 -0300

    Adicionando campos telefone e tipo

--- enviar.php
+++ enviar.php
@@ -1,67 +1,35 @@
 <?php
 
-$nome = $_POST['name'];
-$email = $_POST['email'];
-$massage = $_POST['message'];
+if(isset($_POST['email']) && !empty($_POST['email']))
 
-$arquivo = "
-<style type = 'text/css'>
-body{
-    margin:opx;
-    font-family:Calibri;
-    fonte-size:12px;
-    color:#000000;
-}
-a{
-    color: #666666;
-    text-decoration:none;
-}
-a:hover{
-    color:#FF0000;
-    text-decoration:none;
-}
-</style>
-<html>
-<table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
-<tr>
-<td>
-<tr>
-    <td width='500'>Nome:$nome</td>
-    </tr>
-    <tr>
-    <td width='320'>E-mail:<b>$email</b></td>
-</tr>
-<tr>
-    <td width='320'>Telefone:<b>$telefone</b></td>
-    </tr>
-<tr>
-    <td width='320'>Opções:$escolhas</td>
-</tr>
-<tr>
-    <td width='320'>Mensagem:$nome</td>
-</tr>
-</td>
-</tr>
-<tr>
-<td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
-</tr>
-</table>
-</html>
-";
-$emailenviar = "keila_dcardoso@hotmail.com";
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$telefone = addslashes($_POST['telefone']);
+$tipo = addslashes($_POST['tipo']);
+$message = addslashes($_POST['message']);
+
     $destino = "kd.keke1@gmail.com";
     $assunto = "Contato Paulo Di Mello";
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-            $headers .= 'From: $nome <$email>';
+    $body= "Nome: ".$nome. "\r\n".
+            "Email: ".$email. "\r\n".
+            "Telefone: ".$telefone. "\r\n".
+            "Tipo: ".$tipo. "\r\n".
+            "Mensagem: ".$message. "\r\n".
+            "\r\n".
+            "Olá, obrigado pelo seu contato. Nossa equipe irá retornar o mais breve possível";
 
-    $enviaremail = mail($destino, $assunto, $arquivo, $headers);
-    if($enviaremail){
-    $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-    echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
-    } else {
-    $mgm = "ERRO AO ENVIAR E-MAIL!";
-    echo "";
-    }
+
+    $header = "From:keila_dcardoso@hotmail.com"."\r\n"."Reply-To:".$email."\r\n"
+    ."X=Mailer:PHP/".phpversion();
+
+    if (mail($destino, $assunto, $body, $header)){
+
+    echo("Email enviado!");
+
+}else {
+    echo("Ops! Algo deu errado...");
+}
+
+
 ?>
